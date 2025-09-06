11c70083dcdde8bb2f7b822a808ad47f76eb2946
Talha Karasu <talhakarasu@icloud.com>
2025-09-07 02:55:29 +0300

    aa

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,32 +1,32 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import HomePage from "./Pages/HomePage/HomePage";
 import PortfolioPage from "./Pages/PortfolioPage/PortfolioPage";
 import ContactPage from "./Pages/ContactPage/ContactPage";
 
-const App: React.FC = () => {
+import "./App.css";
+
+function App() {
   return (
     <BrowserRouter>
-      <div className="max-w-4xl mx-auto p-8">
-
+      <div className="app-container">
         {/* Navbar */}
-        <nav className="flex justify-center space-x-6 mb-12 text-lg font-medium">
-          <Link to="/" className="text-blue-600 hover:underline">Home</Link>
-          <Link to="/portfolio" className="text-blue-600 hover:underline">Portfolio</Link>
-          <Link to="/contact" className="text-blue-600 hover:underline">Contact</Link>
+        <nav className="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/contact">Contact</Link>
         </nav>
 
         {/* Routes */}
         <Routes>
           <Route path="/" element={<HomePage />} />
-
           <Route path="/portfolio" element={<PortfolioPage />} />
           <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </div>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
