e5a5149fcf1f433917e715c902ec0155c6bb6a90
Talha Karasu <talhakarasu@icloud.com>
2025-09-10 17:33:35 +0300

    sesi çıkarmış

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
-import ReactAudioPlayer from "react-audio-player";
 import ScrollToTop from "./Components/ScrollToTop";
 import AboutPage from "./Pages/AboutPage/About";
 import PortfolioPage from "./Pages/PortfolioPage/PortfolioPage";
@@ -16,14 +15,6 @@ function App() {
             <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
             <NavLink to="/portfolio" className={({ isActive }) => (isActive ? "active" : "")}>Portfolio</NavLink>
             <NavLink to="/contact" className={({ isActive }) => (isActive ? "active" : "")}>Contact</NavLink>
-          </div>
-
-          <div className="audio-player-wrapper">
-            <ReactAudioPlayer
-              src="https://alemfm.radyotvonline.net/alemfmaac?/;stream.mp3"
-              volume={0.3}
-              controls
-            />
           </div>
         </nav>
 
